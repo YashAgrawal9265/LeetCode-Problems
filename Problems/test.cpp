a60584796528299515d45a78e90a017d3b0f3bc3
yash <yashagrawal9265@gmail.com>
2022-05-20 11:31:26 +0530

    removing problems folder

--- Problems/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define int long long
-using namespace std;
-signed main(){
-    fastIO;
-    
-    return 0;
-}
