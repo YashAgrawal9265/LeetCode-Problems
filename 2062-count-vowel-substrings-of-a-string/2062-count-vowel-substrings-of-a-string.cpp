3b3801e78a277e413e5fbe6203c710b21ceb325c
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-21 02:19:12 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (95.38%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -1,25 +1,37 @@
 class Solution {
-    
 public:
     bool isVowel(char c){
         if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
         return false;
     }
-    int countVowelSubstrings(string str) {
+    int count_at_most_k_vowel(string& str, int k){
         int n = str.size();
+        unordered_map<char,int> mp;
+        int right = 0, left = 0;
         int count = 0;
-        for(int i=0;i<n;i++){
-            unordered_set<char> st;
-            for(int j=i;j<n;j++){
-                if(isVowel(str[j])){
-                    st.insert(str[j]);
-                }
-                else{
-                    break;
-                }
-                if(st.size() == 5) count++;
+        int result = 0;
+        while(right < n){
+            if(!isVowel(str[right])){
+                left = right + 1;
+                right++;
+                mp.clear();
+                count = 0;
+                continue;
             }
+            mp[str[right]]++;
+            if(mp[str[right]] == 1) count++;
+            while(count > k){
+                mp[str[left]]--;
+                if(mp[str[left]] == 0) count--;
+                left++;
+            }
+            result += (right-left+1);
+            right++;
         }
-        return count;
+        return result;
+    }
+    int countVowelSubstrings(string str) {
+        return count_at_most_k_vowel(str,5) - count_at_most_k_vowel(str,4);
+        
     }
 };
