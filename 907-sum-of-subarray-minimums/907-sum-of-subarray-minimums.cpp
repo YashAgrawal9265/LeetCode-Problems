496f2af549776de8af0d47f9df6ecded437c58d6
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-22 15:17:59 +0530

    Time: 81 ms (96.71%), Space: 39.4 MB (97.71%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& arr) {
+    int sumSubarrayMins(vector<int>& nums) {
         int mod = 1e9 + 7;
-        int n = arr.size();
-        vector<int> prev_smaller(n), next_smaller(n);
-        stack<int> pre_st, next_st;
-        for(int i=0;i<n;i++){
-            // calculating previous smaller
-            while(!pre_st.empty() and arr[pre_st.top()] > arr[i]){
-                pre_st.pop();
-            }
-            if(pre_st.empty()) prev_smaller[i] = -1;
-            else prev_smaller[i] = pre_st.top();
-            pre_st.push(i);
-            
-            // calculating next smaller
-            while(!next_st.empty() and arr[next_st.top()] >= arr[n-i-1]){
-               
-                next_st.pop();
-            }
-            if(next_st.empty()) next_smaller[n-i-1] = n;
-            else next_smaller[n-i-1] = next_st.top();
-            next_st.push(n-i-1);
-        }
+        int n = nums.size();
         
-        long long sum = 0;
-        for(int i=0;i<n;i++){
-            int left = i - prev_smaller[i];
-            int right = next_smaller[i] - i;
-            sum = (sum + 1LL*arr[i]*left*right) % mod;
+        // finding minimum element
+        // using next smaller and previous smaller
+        stack<int> st;
+        long long result = 0;
+        for(int i=0;i<=n;i++){
+            while(!st.empty() and (i == n || nums[st.top()] >= nums[i])){
+                int idx = st.top();
+                int right = i - idx;
+                int val = nums[idx];
+                st.pop();
+                int left;
+                if(st.empty()){
+                    left = idx - (-1);
+                }
+                else{
+                    left = idx-st.top();
+                }
+                result = (result + 1LL*right*left*val) % mod;
+            }
+            st.push(i);
         }
-        return sum;
+        return result;
     }
 };
