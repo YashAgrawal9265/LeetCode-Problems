8d34a7f5910e7957b6b810139fdf16a5a9e6b89b
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-23 14:00:41 +0530

    Time: 251 ms (54.44%), Space: 21.5 MB (47.40%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     void dfs(vector<pair<int,int>> adj[], vector<int>& vis, vector<int>& values, int node, int val, int& mxVal, int mxTime){
         if(mxTime < 0) return;
-        // if(node == 3) cout<<mxTime<<" "<<val<<endl;
-        if(node == 0){
-            // cout<<mxTime<<" "<<val<<endl;
+        vis[node]++;
+        // if(vis[node] == 1) val += values[node];
+        if(node == 0){  
             mxVal = max(val,mxVal);
         }
         for(auto it: adj[node]){
             int time = it.second;
             int child = it.first;
             if(!vis[child]){
-                vis[child] = 1;
                 dfs(adj,vis,values,child,val+values[child],mxVal,mxTime-time);
-                vis[child] = 0;
             }
             else{
                 dfs(adj,vis,values,child,val,mxVal,mxTime-time);
             }
         }
+        vis[node]--;
     }
     int maximalPathQuality(vector<int>& values, vector<vector<int>>& edges, int mxTime) {
         int n = values.size();
@@ -30,7 +29,7 @@ public:
         }
         vector<int> vis(n,0);
         int mxVal = INT_MIN;
-        vis[0] = 1;
+        // vis[0] = 1;
         dfs(adj,vis,values,0,values[0],mxVal,mxTime);
         return mxVal;
     }
