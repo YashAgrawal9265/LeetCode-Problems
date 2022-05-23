e9e14ef80d1196c43fe9701eb978d7af949c18b4
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-23 22:57:11 +0530

    Time: 137 ms (94.41%), Space: 26.1 MB (76.76%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,73 +1,50 @@
 class ele{
 public:
-    int val;
+    int cost;
     int node;
     int time;
     ele(int a, int b, int c){
-        val = a;
+        cost = a;
         node = b;
         time = c;
     }
 };
 struct cmp{
     bool operator()(ele a, ele b){
-        return a.val > b.val;
+        return a.cost > b.cost;
     }
 };
 class Solution {
 public:
-    int dfs(vector<pair<int,int>> adj[], vector<int>& vis, vector<int>& values, int node, int mxTime, vector<vector<int>>& dp){
-        if(mxTime < 0) return 1e8;
-        if(node == values.size()-1){
-            return values[values.size()-1];
-        }
-        if(dp[node][mxTime] != -1) return dp[node][mxTime];
-        // vis[node] = 1;
-        int mnVal = 1e8;
-        for(auto it: adj[node]){
-            int child = it.first, time = it.second;
-            // if(!vis[child]){
-                // vis[node] = 1;
-                mnVal = min(mnVal, values[node] + dfs(adj,vis,values,child,mxTime-time,dp)); 
-                // vis[node] = 0;
-            // }
-           
-        }
-        // vis[node] = 0;
-        return dp[node][mxTime] = mnVal;
-    }
     int minCost(int mxTime, vector<vector<int>>& edges, vector<int>& values) {
         int n = values.size();
         vector<pair<int,int>> adj[n];
         for(auto it: edges){
-            int u = it[0], v = it[1], time = it[2];
-            adj[u].push_back({v,time});
-            adj[v].push_back({u,time});
+            int u = it[0], v = it[1], wt = it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
-        vector<int> vis(n,0);
-        vector<vector<int>> dp(n, vector<int>(mxTime+1,-1));
-        int result = dfs(adj,vis,values,0,mxTime,dp);
-        return result >= 1e8 ? -1 : result; 
+        priority_queue<ele,vector<ele>,cmp> pq;
         vector<int> dist(n,INT_MAX);
-        dist[0] = values[0];
-        priority_queue<ele, vector<ele>, cmp> pq;
-        pq.push(ele(values[0],0,mxTime));
+        vector<int> time(n,INT_MAX);
+        dist[0] = values[0], time[0] = 0;
+        pq.push(ele(dist[0],0,0));
         while(!pq.empty()){
-            int node = pq.top().node;
-            int time = pq.top().time;
-            int val = pq.top().val;
+            auto [curDist,curNode,curTime] = pq.top();
             pq.pop();
-            if(dist[node] < val) continue;
-            if(node == n-1) return val;
-            for(auto it: adj[node]){
-                int child = it.first, t = it.second;
-                if(time - t >= 0 and dist[node] + values[child] < dist[child]){
-                    dist[child] = dist[node] + values[child];
-                    pq.push(ele(dist[child],child,time-t));
+            if(curNode == n-1 and curTime <= mxTime) return curDist;
+            if(curTime > mxTime) continue;
+            for(auto [child,wt]: adj[curNode]){
+                int nextDist = curDist + values[child];
+                int nextTime = curTime + wt;
+                if(nextDist < dist[child] || nextTime < time[child]){
+                    dist[child] = nextDist;
+                    time[child] = nextTime;
+                    pq.push({dist[child],child,time[child]});
                 }
             }
-            
         }
         return -1;
+        
     }
 };
