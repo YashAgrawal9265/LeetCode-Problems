5687c1a33cce0cb6d4af9b4c1438fed1202a9d8c
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-24 23:11:28 +0530

    Time: 172 ms (24.24%), Space: 43.4 MB (52.02%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -3,27 +3,13 @@ public:
     string decodeCiphertext(string str, int row) {
         int n = str.size();
         int col = n / row;
-        vector<vector<char>> arr(row, vector<char>(col,' '));
-        int k = 0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                arr[i][j] = str[k++];
-            }
-        }
-        
         string original = "";
-        
-        for(int gap=0;gap<col;gap++){
-            for(int i=0,j=gap;j<col&&i<row;i++,j++){
-                original += arr[i][j];
+        for(int i=0;i<col;i++){
+            for(int j=i;j<n;j+=(col+1)){
+                original += str[j];
             }
         }
-        
-        int x = original.size()-1;
-        while(x >= 0){
-            if(original[x] != ' ') break;
-            x--;
-        }
-        return original.substr(0,x+1);
+        while(original.back() == ' ') original.pop_back();
+        return original;
     }
 };
