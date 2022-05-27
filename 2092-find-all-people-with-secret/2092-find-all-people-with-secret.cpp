97f26ecd0fc1e01a301c98e41070e6d87a3726ce
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-27 16:31:10 +0530

    Time: 1366 ms (24.89%), Space: 181 MB (44.66%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        vector<pair<int,int>> adj[n];
+        for(auto it: meetings){
+            int u = it[0], v = it[1], time = it[2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(n,INT_MAX);
+        pq.push({0,0});
+        pq.push({0,firstPerson});
+        dist[0] = 0;
+        dist[firstPerson] = 0;
+        vector<int> result;
+        
+        while(!pq.empty()){
+            auto [time,node] = pq.top();
+            pq.pop();
+            if(dist[node] < time) continue;
+            // if(vis[node]) continue;
+            // vis[node] = 1;
+            // result.push_back(node);
+            for(auto [child,wt]: adj[node]){
+                // if(child == 4) cout<<time<<" "<<node<<" "<<wt<<endl;
+                if(wt < dist[child] and time <= wt){
+                    dist[child] = wt;
+                    pq.push({wt,child});
+                }
+                
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(dist[i] != INT_MAX) result.push_back(i);
+        }
+        return result;
+    }
+};
