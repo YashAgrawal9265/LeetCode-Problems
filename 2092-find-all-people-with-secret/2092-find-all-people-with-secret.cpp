f11b95778cd634850be81b2c9e0dcc6754c635bb
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-27 16:34:49 +0530

    Time: 683 ms (94.20%), Space: 171.7 MB (59.81%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,39 +1,52 @@
 class Solution {
 public:
-    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<pair<int,int>> adj[n];
-        for(auto it: meetings){
-            int u = it[0], v = it[1], time = it[2];
-            adj[u].push_back({v,time});
-            adj[v].push_back({u,time});
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) { 
+		// create the graph betwen the persons
+        vector<vector<pair<int,int>>> graph(n);
+        for(int i = 0; i < meetings.size(); ++i){
+            graph[meetings[i][0]].push_back({meetings[i][1],meetings[i][2]});
+            graph[meetings[i][1]].push_back({meetings[i][0],meetings[i][2]});
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> dist(n,INT_MAX);
-        pq.push({0,0});
-        pq.push({0,firstPerson});
-        dist[0] = 0;
-        dist[firstPerson] = 0;
-        vector<int> result;
+
+		// using min heap , using time as the sort order key.   
+        priority_queue<pair<int,int> , vector<pair<int,int> > , greater<pair<int,int>> > q;
+		
+		// Add both 0 and firstPerson in the queue as they know the secret initially at time 0 
+        q.push({0, firstPerson});
+        q.push({0, 0});
+		//  vector to store res  
+        vector<int> res;
+		
+		// marking the persons as visited if they have met and shared the secret to all possible persons they could 
+        vector<bool> visited(n,false);
         
-        while(!pq.empty()){
-            auto [time,node] = pq.top();
-            pq.pop();
-            if(dist[node] < time) continue;
-            // if(vis[node]) continue;
-            // vis[node] = 1;
-            // result.push_back(node);
-            for(auto [child,wt]: adj[node]){
-                // if(child == 4) cout<<time<<" "<<node<<" "<<wt<<endl;
-                if(wt < dist[child] and time <= wt){
-                    dist[child] = wt;
-                    pq.push({wt,child});
+        while(!q.empty()){
+            pair<int,int> curr = q.top();
+            q.pop();
+            
+            int person = curr.second;
+            int time = curr.first;
+			
+			// person had already shared the secret with others
+            if(visited[person]) {
+                continue;
+            }
+            visited[person] = true;
+			
+			//iterate all the meetings of this person and add the potential new persons he can share secret with 
+            for(pair<int,int> neigh : graph[person]){
+                if(!visited[neigh.first] && time <= neigh.second){
+                    q.push({neigh.second,neigh.first});
                 }
-                
             }
         }
-        for(int i=0;i<n;i++){
-            if(dist[i] != INT_MAX) result.push_back(i);
+        
+		// all visited persons know the secret
+        for(int i = 0; i < n ; ++i ){
+            if(visited[i]){
+                res.push_back(i);        
+            }
         }
-        return result;
+        return res;
     }
 };
