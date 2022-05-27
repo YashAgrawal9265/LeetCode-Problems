762d982fd1017060dd201531b3fdd6496ec48c47
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-28 00:01:46 +0530

    Time: 7 ms (77.91%), Space: 8.7 MB (86.64%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& mat, int i, int j,int m, int n, vector<vector<int>>& dp){
-        if(i == m-1 and j == n-1){
-            if(mat[i][j] <= 0) return abs(mat[i][j]) + 1;
-            return 1;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = 1e8;
-        if(i+1 < m)
-            down = solve(mat,i+1,j,m,n,dp);
-        int right = 1e8;
-        if(j+1 < n)
-            right = solve(mat,i,j+1,m,n,dp);
-        int result = mat[i][j] - min(right,down);
-        if(result < 0) result = abs(result);
-        else result = 1;
-        return dp[i][j] = result;               
-    }
     int calculateMinimumHP(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> next(n, 0), cur(n,0);
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(i == m-1 and j == n-1){
-                    if(mat[i][j] <= 0) dp[i][j] = abs(mat[i][j]) + 1;
-                    else dp[i][j] = 1;
+                    if(mat[i][j] <= 0) cur[j] = abs(mat[i][j]) + 1;
+                    else cur[j] = 1;
                 }
                 else{
                    int down = 1e8;
                     if(i+1 < m)
-                        down = dp[i+1][j];
+                        down = next[j];
                     int right = 1e8;
                     if(j+1 < n)
-                        right = dp[i][j+1];
+                        right = cur[j+1];
                     int result = mat[i][j] - min(right,down);
                     if(result < 0) result = abs(result);
                     else result = 1;
-                    dp[i][j] = result;  
+                    cur[j] = result;  
                 }
             }
+            next = cur;
         } 
-        return dp[0][0];
+        return next[0];
     }
 };
