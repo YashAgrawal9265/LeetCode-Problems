9b4f274dc63bed7d8d546220b68ab90e5dfbd038
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-28 21:38:18 +0530

    Time: 604 ms (50.00%), Space: 129.1 MB (50.00%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -8,21 +8,10 @@ public:
             degree[u]++;
             degree[v]++;
         }
-        vector<pair<int,int>> temp;
-        for(int i=0;i<n;i++){
-            temp.push_back({degree[i],i});
-        }
-        sort(temp.begin(),temp.end());
-        vector<int> values(n);
-        int val = 1;
-        for(auto it: temp){
-            values[it.second] = val++;
-        }
+        sort(degree.begin(),degree.end());
         long long result = 0;
-        for(auto it: edges){
-            int u = it[0], v= it[1];
-            result += values[u];
-            result += values[v];
+        for(int i=0;i<n;i++){
+            result += 1LL*(i+1)*degree[i];
         }
         return result;
         
