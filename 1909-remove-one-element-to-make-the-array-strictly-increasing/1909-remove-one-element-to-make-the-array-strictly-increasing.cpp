15743b8f1e97e543e074a4092920664851ac773b
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-29 19:58:58 +0530

    Time: 17 ms (25.53%), Space: 9.9 MB (60.52%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums){
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] <= nums[i-1]) return false;
+    bool canBeIncreasing(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        bool used = false;
+        for(int i=1;i<n;i++){
+            if(nums[i] <= prev){
+                if(used) return false;
+                used = true;
+                if(i == 1 || nums[i] > nums[i-2]) prev = nums[i];
+                else prev = nums[i-1];
+            }
+            else{
+                prev = nums[i];
+            }
         }
         return true;
-    }
-    bool canBeIncreasing(vector<int>& nums) {
-        int i = 0;
-        for(auto it =  nums.begin(); it != nums.end(); it++){
-            int x = *it;
-            nums.erase(it);
-            if(isValid(nums)) return true;
-            nums.insert(nums.begin()+i,x);
-            i++;
-        }
-        return false;
     }
 };
