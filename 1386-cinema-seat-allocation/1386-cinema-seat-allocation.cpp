8bdaaa2fab42ae6c1b240edcc5ea98de502b6667
Yash Agrawal <yashagrawal9265@gmail.com>
2022-05-30 13:17:12 +0530

    Time: 293 ms (40.21%), Space: 64 MB (37.98%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
-    int maxNumberOfFamilies(int n, vector<vector<int>>& arr) {
-        sort(arr.begin(),arr.end());
-        int result = 0;
-        int visRow = 0;
-        for(int idx=0;idx<arr.size();idx++){
-            int row = arr[idx][0];
-            visRow++;
-            vector<int> temp(11,0);
-            int count = 0;
-            int j;
-            for(j=idx;j<arr.size();j++){
-                if(row != arr[j][0]){
-                    idx = j-1;
-                    break;
-                }
-                temp[arr[j][1]] = 1;
-            }
-            if(j == arr.size()) idx = j-1;
-            bool flag = true;
-            for(int i=2;i<=5;i++){
-                if(temp[i] == 1){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) count++;
-            flag = true;
-            for(int i=6;i<=9;i++){
-                if(temp[i] == 1){
-                    
-                    flag = false;
-                    break;
-                }
-                
-            }
-            if(flag) count++;
-            flag = true;
-            if(count == 0){
-                for(int i=4;i<=7;i++){
-                    if(temp[i] == 1){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag) count++;
-            }
-            
-            result += count;
-            
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int,long> mp;
+        for(auto it: reservedSeats){
+            int row = it[0], seat = it[1];
+            mp[row] |= (1<<(10-seat));
         }
-        result += (n-visRow)*2;
+       
+        int result = 0;
+        for(auto it: mp){
+            int count = 0;
+            if(!(it.second & 480)) count++;
+            if(!(it.second & 30)) count++;
+            if(count == 0 and !(it.second & 120)) count++;
+            result += count;
+        }
+       
+        result += (n-mp.size())*2;
         return result;
     }
 };
