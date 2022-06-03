24f4222cd25583bd7e79555f002b62e42cf66e81
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-03 21:08:30 +0530

    Time: 70 ms (48.41%), Space: 20.8 MB (6.17%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        int n = digits.size();
-        vector<pair<int,int>> even;
-        int idx = 0;
-        for(auto &it: digits){
-            if(it % 2 == 0){
-                even.push_back({it,idx});
-            }
-            idx++;
+        unordered_map<char,int> mp;
+        for(auto it: digits){
+            mp[it+'0']++;
         }
-        unordered_set<int> st;
-        for(int i=0;i<n;i++){
-            if(digits[i] == 0) continue;
-            string temp(3,'#');
-            temp[0] = digits[i] + '0';
-            for(int j=0;j<even.size();j++){
-                temp[2] = even[j].first + '0';
-                int idx = even[j].second;
-                if(idx == i) continue;
-                for(int k=0;k<n;k++){
-                    if(k != idx and k != i){
-                        temp[1] = digits[k] + '0';
-                        st.insert(stoi(temp));
+        vector<int> result;
+        for(int i=100;i<=999;i++){
+            if(i % 2 == 0){
+                
+                string str = to_string(i);    
+                unordered_map<char,int> mp2;
+                mp2[str[0]]++;
+                mp2[str[1]]++;
+                mp2[str[2]]++;
+                bool flag = true;
+                for(auto &it: mp2){
+                    if(mp[it.first] < it.second){
+                        flag = false;
+                        break;
                     }
                 }
+                if(flag){
+                    result.push_back(i);
+                }
             }
+            
         }
-        vector<int> result(st.begin(),st.end());
-        sort(result.begin(),result.end());
         return result;
     }
 };
