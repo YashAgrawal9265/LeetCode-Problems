56267ed34d2700f419d0055f655c78dceb547373
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-05 22:39:01 +0530

    Time: 26 ms (41.35%), Space: 6.6 MB (41.35%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -3,11 +3,13 @@ public:
     int minSteps(int n) {
         vector<int> dp(n+1);
         dp[1] = 0;
-      
         for(int i=2;i<=n;i++){
             dp[i] = i;
             for(int j=i-1;j>1;j--){
-                if(i%j == 0) dp[i] = min(dp[i],dp[j] + i/j);
+                if(i%j == 0){
+                    dp[i] = min(dp[i],dp[j] + i/j);
+                    break;
+                }
             }
         }
         return dp[n];
