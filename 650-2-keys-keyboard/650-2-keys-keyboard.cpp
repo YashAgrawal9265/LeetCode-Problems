dd4f4e2d374f05e3f4d3881855fc351a96c9e63f
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-05 22:38:42 +0530

    Time: 39 ms (31.24%), Space: 6.6 MB (50.71%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int solve(int screen, int n, int clipboard, int canCopy, vector<vector<int>>& dp){
-        if(screen > n) return 1e9;
-        if(screen == n) return 0;
-        if(dp[screen][clipboard] != -1) return dp[screen][clipboard];
-        int copy = 1e9, paste = 1e9;
-        if(clipboard == 0){
-            copy = 1 + solve(screen,n,screen,0,dp);
-            return dp[screen][clipboard] = min(copy,paste);
-        }
-        else{
-            if(screen != clipboard) copy = 1+ solve(screen,n,screen,0,dp);
-            paste = 1 + solve(screen+clipboard,n,clipboard,1,dp);
-            return dp[screen][clipboard] = min(copy,paste);
-        }
-    }
     int minSteps(int n) {
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        return solve(1,n,0,1,dp);
+        vector<int> dp(n+1);
+        dp[1] = 0;
+      
+        for(int i=2;i<=n;i++){
+            dp[i] = i;
+            for(int j=i-1;j>1;j--){
+                if(i%j == 0) dp[i] = min(dp[i],dp[j] + i/j);
+            }
+        }
+        return dp[n];
     }
 };
