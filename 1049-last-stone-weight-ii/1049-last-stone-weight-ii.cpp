53da5f8740b7601463fc882b959c5b197052dde0
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-06 22:55:42 +0530

    Time: 16 ms (19.40%), Space: 10.4 MB (15.22%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    vector<vector<bool>> solve(int n, int k, vector<int> &arr)
-    {
-        vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
-        for (int i = 0; i < n; i++)
-        {
-            dp[i][0] = true;
-        }
-        if (arr[0] <= k)
-            dp[0][arr[0]] = true;
-        for (int idx = 1; idx < n; idx++)
-        {
-            for (int target = 1; target <= k; target++)
-            {
-                int notake = dp[idx - 1][target];
-                int take = false;
-                if (arr[idx] <= target)
-                    take = dp[idx - 1][target - arr[idx]];
-                dp[idx][target] = notake | take;
-            }
-        }
-        return dp;
-    }
     int lastStoneWeightII(vector<int>& stones) {
         int sum = 0;
         int n = stones.size();
         for(auto it: stones){
             sum += it;
         }
-        vector<vector<bool>> dp = solve(n, sum,stones);
+        vector<vector<int>> dp(n, vector<int>(sum+1,0));
+        dp[0][0] = 1;
+        dp[0][stones[0]] = 1;
+        for(int idx=1;idx<n;idx++){
+            for(int target=0;target<=sum;target++){
+                int nopick = dp[idx-1][target];
+                int pick = 0;
+                if(stones[idx] <= target){
+                    pick = dp[idx-1][target-stones[idx]];
+                }
+                dp[idx][target] = (nopick or pick);
+            }
+        }
+        
         int result = INT_MAX;
         for(int i=0;i<=sum/2;i++){
             if(dp[n-1][i]){
