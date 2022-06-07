0d9f093c5c418870e346ca234cc22bacadf11e5a
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-07 15:10:57 +0530

    Time: 10 ms (7.23%), Space: 6.7 MB (35.06%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -13,9 +13,21 @@ public:
         }
         return dp[target] = count;
     }
-    int combinationSum4(vector<int>& nums, int target) {
+    int combinationSum4(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> dp(target+1,-1);
-        return solve(nums,target,n,dp);
+        
+        vector<unsigned> dp(k+1,-1);
+        // return solve(nums,k,n,dp);
+        dp[0] = 1;
+        for(int target=1;target<=k;target++){
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(nums[i] <= target){
+                    count += dp[target-nums[i]];
+                }
+            }
+            dp[target] = count;
+        }
+        return dp[k];
     }
 };
