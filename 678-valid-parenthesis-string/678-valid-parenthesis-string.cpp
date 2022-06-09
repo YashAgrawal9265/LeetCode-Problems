70f26009387e4258a3c44623cb81f257888eaeea
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-09 20:53:50 +0530

    Time: 15 ms (9.03%), Space: 8.1 MB (10.01%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int solve(string& str, int idx, int n, int open,vector<vector<int>>& dp){
         if(open < 0) return false;
-        if(dp[idx][open] != -1) return dp[idx][open];
         if(idx == n){
             if(open == 0) return true;
             return false;
         }
+        if(dp[idx][open] != -1) return dp[idx][open];
         if(str[idx] == '(') return dp[idx][open] = solve(str,idx+1,n,open+1,dp);
         else if(str[idx] == ')') return dp[idx][open] = solve(str,idx+1,n,open-1,dp);
         else{
@@ -19,7 +19,25 @@ public:
     }
     bool checkValidString(string str) {
         int n= str.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        return solve(str,0,n,0,dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        dp[n][0] = 1;
+        for(int idx=n-1;idx>=0;idx--){
+            for(int open=0;open<n;open++){
+                if(str[idx] == '(') dp[idx][open] = dp[idx+1][open+1];
+                else if(str[idx] == ')'){
+                    if(open - 1 >= 0){
+                       dp[idx][open] = dp[idx+1][open-1]; 
+                    }
+                }
+                else{
+                    int op1 = dp[idx+1][open+1];
+                    int op2 = (open -1 >= 0) ? dp[idx+1][open-1] : 0;
+                    int op3 = dp[idx+1][open];
+
+                    dp[idx][open] = op1 || op2 || op3;
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
