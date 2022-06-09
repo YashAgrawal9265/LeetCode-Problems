9980e06e4f6ab2f365a4b327afc7085bdac466e8
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-09 22:44:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (58.63%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     bool checkValidString(string str) {
         int n = str.size();
-        int openMn = 0, openMx = 0;
+        int balance = 0;
         for(int i=0;i<n;i++){
-            if(str[i] == '('){
-                openMn++, openMx++;
-            }
-            else if(str[i] == ')'){
-                openMn--, openMx--;
-            }
-            else{
-                openMn--;
-                openMx++;
-            }
-            if(openMx < 0) return false;
-            if(openMn < 0) openMn = 0;
+            if(str[i] == '(' || str[i] == '*') balance++;
+            else balance--;
+            if(balance < 0) return false;
         }
-        if(openMn == 0) return true;
-        return false;
+        if(balance == 0) return true;
+        balance = 0;
+        for(int i=n-1;i>=0;i--){
+            if(str[i] == ')' || str[i] == '*') balance++;
+            else balance--;
+            if(balance < 0) return false;
+        }
+        return true;
     }
 };
