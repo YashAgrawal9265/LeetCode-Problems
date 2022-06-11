bfbbfd878261df8a5e75b3a9f57d2cfe0a468bb6
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-11 11:50:36 +0530

    Time: 280 ms (57.99%), Space: 98.5 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int target) {
+    int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-        vector<int> prefix(n+1,0), suffix(n+1,0);
-        for(int i=1;i<=n;i++){
-            prefix[i] = prefix[i-1] + nums[i-1];
+        int totSum = 0;
+        for(int i=0;i<n;i++){
+            totSum += nums[i];
         }
-        for(int i=n-1;i>=0;i--){
-            suffix[i] = suffix[i+1] + nums[i];
-        }
-        if(suffix[0] == target) return n;
-        int left = 0 , right = 0;
-        int windowSize = -1;
+        if(totSum == x) return n;
+        int target = totSum - x;
+        int right = 0, left = 0;
+        int sum = 0;
+        int result = -1;
         while(right < n){
-           
-            int sum = prefix[left] + suffix[right+1];
-            while(sum < target and left < right){
+            sum += nums[right];
+            while(sum > target and left < right){
+                sum -= nums[left];
                 left++;
-                sum = prefix[left] + suffix[right+1];
             }
-            if(sum == target){
-                windowSize = max(windowSize,right-left+1);
-            }
+            if(sum == target) result = max(result,right-left+1);
             right++;
         }
-        
-        if(windowSize == -1) return -1;
-        return n - windowSize;
-        
+        if(result == -1) return -1;
+        return n - result;
     }
 };
