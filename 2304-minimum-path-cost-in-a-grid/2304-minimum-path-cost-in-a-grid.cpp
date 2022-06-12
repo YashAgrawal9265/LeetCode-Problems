0dacbd6a048be342f6ca8bc10a6dc6ce66397298
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-12 11:59:57 +0530

    Time: 814 ms (100.00%), Space: 79.9 MB (100.00%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -4,23 +4,24 @@ public:
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        for(int j=0;j<n;j++) dp[m-1][j] = grid[m-1][j];
+        vector<int> next(n,0), cur(n,0);
+        for(int j=0;j<n;j++) next[j] = grid[m-1][j];
         for(int i=m-2;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 int mini = 1e8;
                 if(i + 1 <= m-1){
                     
                     for(int k=0;k<n;k++){
-                        mini = min(mini, moveCost[grid[i][j]][k] + grid[i][j] + dp[i+1][k]);
+                        mini = min(mini, moveCost[grid[i][j]][k] + grid[i][j] + next[k]);
                     }
                 }
-                dp[i][j] = mini;
+                cur[j] = mini;
             }
+            next = cur;
         }
         int result = 1e8;
         for(int j=0;j<n;j++){
-            result = min(result,dp[0][j]);
+            result = min(result,next[j]);
         }
         return result;
         
