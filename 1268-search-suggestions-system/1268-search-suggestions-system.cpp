a29a7fea01b76f377c727fcaa20a9db889d46233
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-19 14:38:47 +0530

    Time: 44 ms (96.08%), Space: 26.4 MB (85.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,80 +1,18 @@
-struct Node
-{
-    Node *links[26];
-    vector<string> arr;
-
-    bool containsKey(char ch)
-    {
-        if (links[ch - 'a'] == NULL)
-            return false;
-        return true;
-    }
-   
-    void put(char ch, Node *&node)
-    {
-        links[ch - 'a'] = node;
-    }
-    
-    Node *get(char ch)
-    {
-        return links[ch - 'a'];
-    }
-};
-
-class Trie
-{
-private:
-    Node *root;
-
-public:
-    Trie()
-    {
-        root = new Node();
-    }
-
-    void insert(string word)
-    {
-        Node *node = root;
-        for (int i = 0; i < word.size(); i++)
-        {
-            if (!node->containsKey(word[i]))
-            {
-                Node *newNode = new Node();
-                node->put(word[i], newNode);
-            }
-            node = node->get(word[i]);
-            if(node->arr.size() < 3)
-                node->arr.push_back(word);
-        }
-        
-    }
-
-    vector<string> search(string word)
-    {
-        Node *node = root;
-        for (int i = 0; i < word.size(); i++)
-        {
-            if (!node->containsKey(word[i]))
-            {
-                return {};
-            }
-            node = node->get(word[i]);
-        }
-        return node->arr;
-    }
-};
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        Trie trie;
         sort(products.begin(),products.end());
-        for(auto it: products) trie.insert(it);
-        string temp = "";
+        int n = products.size();
         vector<vector<string>> result;
+        string prefix = "";
         for(int i=0;i<searchWord.size();i++){
-            temp += searchWord[i];
-            result.push_back(trie.search(temp));
+            prefix += searchWord[i];
+            int idx = lower_bound(products.begin(),products.end(),prefix) - products.begin();
+            vector<string> temp;
+            for(int j=idx;j<min(idx+3,n) && products[j].compare(0,prefix.size(),prefix) == 0;j++){
+                temp.push_back(products[j]);
+            }
+            result.push_back(temp);
         }
         return result;
     }
