fd76bb1d0a8b9bda858f5cae9ac52e8b6e4ce350
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-19 13:19:35 +0530

    Time: 346 ms (26.40%), Space: 88.6 MB (35.83%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        unordered_map<string,set<string>> mp;
+        sort(products.begin(),products.end());
+        unordered_map<string,vector<string>> mp;
         for(auto &it: products){
             string temp = "";
             for(int i=0;i<it.size();i++){
                 temp += it[i];
-                mp[temp].insert(it);
+                if(mp[temp].size() == 3) continue;
+                mp[temp].push_back(it);
             }
         }
         vector<vector<string>> result;
         string temp = "";
         for(int i=0;i<searchWord.size();i++){
             temp += searchWord[i];
-            vector<string> arr;
-            int k = 3;
-            auto &st = mp[temp];
-            for(auto it = st.begin(); it != st.end() and k; it++){
-                k--;
-                arr.push_back(*it);
-            }
-            result.push_back(arr);
+            result.push_back(mp[temp]);
         }
         return result;
     }
