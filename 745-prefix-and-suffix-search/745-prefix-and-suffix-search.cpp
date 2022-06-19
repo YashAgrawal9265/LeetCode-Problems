5220bc44914c636f55c3deb5d8608b6dbbfb7147
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-19 12:36:40 +0530

    Time: 609 ms (64.29%), Space: 185.3 MB (41.36%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,27 +1,67 @@
+struct Node{
+    Node* links[27];
+    int idx;
+    
+    bool containsKey(char ch){
+        if(links[ch-'a'] == NULL) return false;
+        return true;
+    }
+    void put(char ch, Node*& node){
+        links[ch-'a'] = node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+};
+class Trie{
+private:
+    Node* root;
+public:
+    Trie(){
+        root = new Node();
+    }
+    void insert(string& word, int index){
+        Node* node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containsKey(word[i])){
+                Node* newNode = new Node();
+                node->put(word[i],newNode);
+            }
+            node = node->get(word[i]);
+            node->idx = index;
+        }
+    }
+    int search(string word){
+        Node* node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containsKey(word[i])){
+                return -1;
+            }
+            node = node->get(word[i]);
+        }
+        return node->idx;
+    }
+};
 class WordFilter {
 private:
-    unordered_map<string,int> mp;
+    Trie trie;
 public:
     WordFilter(vector<string>& words) {
         int idx = 0;
         for(auto &it: words){
             int n = it.size();
             string str = it;
-            for(int i=0;i<n;i++){
-                string pre = str.substr(0,i+1);
-                for(int j=n-1;j>=0;j--){
-                    string suf = str.substr(j);
-                    mp[pre + "#" + suf] = idx;
-                }
+            for(int i = n-1;i>=0;i--){
+                string temp = str.substr(i) + "{" + str;
+                trie.insert(temp,idx);
             }
             idx++;
         }
     }
     
     int f(string prefix, string suffix) {
-        string temp = prefix + "#" + suffix;
-        if(mp.find(temp) == mp.end()) return -1;
-        return mp[temp];
+        return trie.search(suffix + "{" + prefix);
+        
     }
 };
 
