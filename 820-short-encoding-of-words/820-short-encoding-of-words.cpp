956ef3ba4987b86203e7dba087d55518207b0c82
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-20 19:59:21 +0530

    Time: 142 ms (40.60%), Space: 73.8 MB (29.70%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,3 +1,69 @@
+struct Node
+{
+    Node *links[26];
+   
+
+    bool containsKey(char ch)
+    {
+        if (links[ch - 'a'] == NULL)
+            return false;
+        return true;
+    }
+    
+    void put(char ch, Node *&node)
+    {
+        links[ch - 'a'] = node;
+    }
+    
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                Node *newNode = new Node();
+                node->put(word[i], newNode);
+            }
+            node = node->get(word[i]);
+        }
+        
+    }
+
+    bool search(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return true;
+        
+    }
+
+};
+
 class Solution {
 public:
     bool static cmp(string& a, string& b){
@@ -5,18 +71,13 @@ public:
     }
     int minimumLengthEncoding(vector<string>& words) {
         sort(words.begin(),words.end(),cmp);
-        unordered_set<string> st;
+        Trie trie;
         int result = 0;
-        for(int i=0;i<words.size();i++){
-            string str = words[i];
-            if(st.find(str) != st.end()) continue;
-            // cout<<str<<endl;
-            int start = str.size()-1;
-            while(start >= 0){
-                st.insert(str.substr(start));
-                start--;
-            }
-            result += str.size() + 1;
+        for(auto it: words){
+            reverse(it.begin(),it.end());
+            if(trie.search(it)) continue;
+            trie.insert(it);
+            result += it.size() + 1;
         }
         return result;
     }
