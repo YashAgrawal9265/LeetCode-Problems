dba0b7ef5d389c10d5564728e5a2340a27b67267
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-20 10:32:06 +0530

    Time: 58 ms (85.10%), Space: 26.4 MB (85.87%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -5,14 +5,16 @@ public:
         int n = products.size();
         vector<vector<string>> result;
         string prefix = "";
+        int start = 0;
         for(int i=0;i<searchWord.size();i++){
             prefix += searchWord[i];
-            int idx = lower_bound(products.begin(),products.end(),prefix) - products.begin();
+            int idx = lower_bound(products.begin()+start,products.end(),prefix) - products.begin();
             vector<string> temp;
             for(int j=idx;j<min(idx+3,n) && products[j].compare(0,prefix.size(),prefix) == 0;j++){
                 temp.push_back(products[j]);
             }
             result.push_back(temp);
+            start = idx;
         }
         return result;
     }
