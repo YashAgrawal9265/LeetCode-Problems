7d862e5789bb05595fd867f32db98782c20eb06c
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-20 20:33:38 +0530

    Time: 81 ms (67.82%), Space: 17.4 MB (81.19%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,83 +1,19 @@
-struct Node
-{
-    Node *links[26];
-   
-
-    bool containsKey(char ch)
-    {
-        if (links[ch - 'a'] == NULL)
-            return false;
-        return true;
-    }
-    
-    void put(char ch, Node *&node)
-    {
-        links[ch - 'a'] = node;
-    }
-    
-    Node *get(char ch)
-    {
-        return links[ch - 'a'];
-    }
-};
-
-class Trie
-{
-private:
-    Node *root;
-
-public:
-    Trie()
-    {
-        root = new Node();
-    }
-
-    void insert(string word)
-    {
-        Node *node = root;
-        for (int i = 0; i < word.size(); i++)
-        {
-            if (!node->containsKey(word[i]))
-            {
-                Node *newNode = new Node();
-                node->put(word[i], newNode);
-            }
-            node = node->get(word[i]);
-        }
-        
-    }
-
-    bool search(string word)
-    {
-        Node *node = root;
-        for (int i = 0; i < word.size(); i++)
-        {
-            if (!node->containsKey(word[i]))
-            {
-                return false;
-            }
-            node = node->get(word[i]);
-        }
-        return true;
-        
-    }
-
-};
-
 class Solution {
 public:
-    bool static cmp(string& a, string& b){
-        return a.size() > b.size();
-    }
     int minimumLengthEncoding(vector<string>& words) {
-        sort(words.begin(),words.end(),cmp);
-        Trie trie;
-        int result = 0;
+        unordered_set<string> st(words.begin(),words.end());
         for(auto it: words){
-            reverse(it.begin(),it.end());
-            if(trie.search(it)) continue;
-            trie.insert(it);
-            result += it.size() + 1;
+            string str = it;
+            for(int i=1;i<str.size();i++){
+                string temp = str.substr(i);
+                if(st.find(temp) != st.end()){
+                    st.erase(temp);
+                }
+            }
+        }
+        int result = 0;
+        for(auto it = st.begin();it != st.end();it++){
+            result += it->size() + 1;
         }
         return result;
     }
