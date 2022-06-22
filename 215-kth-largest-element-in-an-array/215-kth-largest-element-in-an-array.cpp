1c92b5826290d74fa2f3b6e9dbd4791f7cba7b38
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-22 11:34:14 +0530

    Time: 176 ms (6.20%), Space: 10.3 MB (15.68%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(auto it: nums){
-            pq.push(it);
-            if(pq.size() > k) pq.pop();
+    int partition(vector<int>& arr, int l, int r){
+        int pivot = arr[r];
+        int i = l, j = l;
+        while(j < r){
+            if(arr[j] <= pivot){
+                swap(arr[i],arr[j]);
+                i++,j++;
+            }
+            else{
+                j++;
+            }
         }
-        return pq.top();
+        swap(arr[i],arr[r]);
+        return i;
+    }
+    void solve(vector<int>& arr, int l ,int r, int k, int& result){
+        if(l <= r){
+            int p = partition(arr,l,r);
+            if(p == arr.size() - k){
+                result = arr[p];
+                return;
+            }
+            solve(arr,l,p-1,k,result);
+            solve(arr,p+1,r,k,result);
+        }
+    }
+    int findKthLargest(vector<int>& arr, int k) {
+        int result = 0;
+        solve(arr,0,arr.size()-1,k,result);
+        return result;
     }
 };
