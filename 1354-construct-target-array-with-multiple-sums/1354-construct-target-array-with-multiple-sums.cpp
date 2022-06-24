7d18c14e29767ec3cdc1794be66048162b2e01cb
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-24 14:34:33 +0530

    Time: 70 ms (42.66%), Space: 30.2 MB (37.06%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -4,16 +4,21 @@ public:
     bool isPossible(vector<int>& target) {
         long long sum = 0;
         int mxIdx = 0;
+        priority_queue<int> pq;
         for(int i=0;i<target.size();i++){
             sum += target[i];
-            if(target[i] > target[mxIdx]){
-                mxIdx = i;
-            }
+            pq.push(target[i]);
         }
-        long long diff = sum - target[mxIdx];
-        if(target[mxIdx] == 1 || diff == 1) return true;
-        if(target[mxIdx] <= diff || diff == 0 || target[mxIdx] % diff == 0) return false;
-        target[mxIdx] %= diff;
-        return isPossible(target);
+        while(!pq.empty() and pq.top() != 1){
+            int val = pq.top();
+            pq.pop();
+            long long diff = sum - val;
+            if(val == 1 || diff == 1) return true;
+            if(val <= diff || diff == 0 || val % diff == 0) return false;
+            val %= diff;
+            pq.push(val);
+            sum = diff + val;
+        }
+        return true;
     }
 };
