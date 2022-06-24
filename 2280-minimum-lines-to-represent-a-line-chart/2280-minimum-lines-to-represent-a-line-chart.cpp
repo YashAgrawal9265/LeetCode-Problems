bc5717a3a6ad95508d1683c3f31c81a9a7c23ecd
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-24 18:45:23 +0530

    Time: 791 ms (32.27%), Space: 100.8 MB (19.76%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int gcd(int a, int b){
-        if(b == 0) return a;
-        return gcd(b,a%b);
-    }
     int minimumLines(vector<vector<int>>& arr) {
         sort(arr.begin(),arr.end());
-        int n = arr.size();
-        if(n == 1) return 0;
-        int p1 = arr[1][1] - arr[0][1];
-        int p2 = arr[1][0] - arr[0][0];
+        int n=arr.size();
+        int count=0;
+        long double  prev=(long double)INT_MAX;
+       
         
-        int d = gcd(abs(p1),abs(p2));
-        int dy = p1/d;
-        int dx = p2/d;
-        
-        int result = 1;
-        for(int i=2;i<n;i++){
-            p1 = arr[i][1] - arr[i-1][1];
-            p2 = arr[i][0] - arr[i-1][0];
-            d = gcd(abs(p1),abs(p2));
-            p1 /= d, p2 /= d;
-            if(p1 != dy || p2 != dx){
-                dy = p1;
-                dx = p2;
-                result++;
+        for(int i=1;i<n;i++)
+        {
+            long double   y2=arr[i][1],x2=arr[i][0];
+            long double  y1=arr[i-1][1],x1=arr[i-1][0];
+            
+            long double   m=(y2-y1)/(x2-x1);
+            if(m!=prev)
+            {
+                count++;
+                prev=m;
             }
+            
         }
-        return result;
+        return count;
+        
     }
 };
