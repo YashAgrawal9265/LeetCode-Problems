75146c60d0639275132712748463fc7313e4cd7c
Yash Agrawal <yashagrawal9265@gmail.com>
2022-06-30 22:13:10 +0530

    Time: 564 ms (54.79%), Space: 102.2 MB (75.69%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingre, vector<string>& supplies) {
         unordered_set<string> supp(supplies.begin(),supplies.end());
-        
-        vector<string> result;
-        
         int n = recipes.size();
-        vector<int> vis(n,0);
-        int x = supp.size();
-        while(true){
-            for(int i=0;i<n;i++){
-                if(vis[i]) continue;
-                bool flag = true;
-                for(auto &it: ingredients[i]){
-                    if(supp.find(it) == supp.end()){
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag){
-                    vis[i] = 1;
-                    result.push_back(recipes[i]);
-                    supp.insert(recipes[i]);
-                }
-            }
-            if(supp.size() == x) break;
-            else x = supp.size();
+        unordered_map<string,vector<string>> adj;
+        unordered_map<string,int> indegree;
+        for(auto &it: recipes){
+            indegree[it] = 0;
         }
         
+        for(int i=0;i<n;i++){
+            for(auto &it: ingre[i]){
+                if(supp.find(it) == supp.end()){
+                    adj[it].push_back(recipes[i]);
+                    indegree[recipes[i]]++;
+                }
+            }
+        }
+        vector<string> result;
+        queue<string> q;
+        for(auto &it: indegree){
+            if(it.second == 0){
+                q.push(it.first);
+                result.push_back(it.first);
+            }
+        }
+        
+        while(!q.empty()){
+            string node = q.front();
+            q.pop();
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                    result.push_back(it);
+                }
+            }
+        }
         return result;
     }
 };
