d2efa144572fcdee8e85ab20dc584ba28fd593e2
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-05 02:22:49 +0530

    Added solution - LeetHub

--- Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.cpp
@@ -10,21 +10,32 @@ class Solution{
 public:
     int mod = 1e9 + 7;
     long long dp[100009][2];
-	long long solve(int n, bool one){
-	    if(n == 0){
-	        return 1;
-	    }
-	    if(dp[n][one] != -1) return dp[n][one];
-	    long long pick = 0;
-	    if(one){
-	        pick = solve(n-1,false) % mod;
-	    }
-	    long long nopick = solve(n-1,true) % mod;
-	    return dp[n][one] = (pick + nopick) % mod;
-	}
+// 	long long solve(int n, bool one){
+// 	    if(n == 0){
+// 	        return 1;
+// 	    }
+// 	    if(dp[n][one] != -1) return dp[n][one];
+// 	    long long pick = 0;
+// 	    if(one){
+// 	        pick = solve(n-1,false) % mod;
+// 	    }
+// 	    long long nopick = solve(n-1,true) % mod;
+// 	    return dp[n][one] = (pick + nopick) % mod;
+// 	}
 	ll countStrings(int n) {
-	    memset(dp,-1,sizeof(dp));
-	    return solve(n,true);
+	    memset(dp,0,sizeof(dp));
+	    dp[0][0] = 1, dp[0][1] = 1;
+	    for(int i=1;i<=n;i++){
+	        for(int one = 0; one <=1; one++){
+	            long long pick = 0;
+        	    if(one){
+        	        pick = dp[i-1][0] % mod;
+        	    }
+        	    long long nopick = dp[i-1][1] % mod;
+        	    dp[i][one] = (pick + nopick) % mod;
+	        }
+	    }
+	    return dp[n][1];
 	}
 };
 
