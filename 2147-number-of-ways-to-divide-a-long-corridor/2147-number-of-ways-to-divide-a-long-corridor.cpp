ee155b1cc9ffcc20d081c951108aa0f0e5a8f1fa
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-07 01:47:01 +0530

    Time: 173 ms (62.13%), Space: 55.6 MB (23.73%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,36 +1,17 @@
 class Solution {
-private:
-    int mod = 1e9 + 7;
 public:
-    long long solve(string& str, int idx, int seats, vector<vector<int>>& dp){
-        if(idx == str.size()){
-            if(seats == 2) return 1;
-            return 0;
-        }
-        if(dp[idx][seats] != -1) return dp[idx][seats];
-        long long count = 0;
-        if(seats == 2){
-            if(str[idx] == 'P'){
-                count += solve(str,idx+1,0,dp);
-                count %= mod;
-                count += solve(str,idx+1,seats,dp);
-                count %= mod;
-            }
-            else{
-                count += solve(str,idx+1,1,dp);
-                count %= mod;
-            }
-        }
-        else{
-            if(str[idx] == 'S') count += solve(str,idx+1,seats + 1,dp);
-            else count += solve(str,idx+1,seats,dp);
-            count %= mod;
-        }
-        return dp[idx][seats] = count;
-    }
     int numberOfWays(string str) {
+        int mod = 1e9 + 7;
         int n = str.size();
-        vector<vector<int>> dp(n+1, vector<int>(3,-1));
-        return solve(str,0,0,dp);
+        vector<int> seats;
+        for(int i=0;i<n;i++){
+            if(str[i] == 'S') seats.push_back(i);
+        }
+        if(seats.size() == 0 || seats.size() % 2) return 0;
+        int result = 1;
+        for(int i=2;i<seats.size();i+=2){
+            result = (1LL* result * (seats[i] - seats[i-1])) % mod;
+        }
+        return result;
     }
 };
