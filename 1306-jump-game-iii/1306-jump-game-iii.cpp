7b7ab28a19cf792a1f7e866c926b1ba0b76664ad
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-09 11:37:22 +0530

    Time: 89 ms (27.54%), Space: 35.9 MB (28.51%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<int>& arr, vector<int>& vis, int idx, int n){
+        vis[idx] = 1;
+        if(arr[idx] == 0) return true;
+        bool result = false;
+        for(int i=0;i<2;i++){
+            int new_idx;
+            if(i == 0) new_idx = idx + arr[idx];
+            else new_idx = idx - arr[idx];
+            if(new_idx >= 0 and new_idx < n and !vis[new_idx]){
+                result = result or solve(arr,vis,new_idx,n);
+                if(result) break;
+            }
+        }
+        return result;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<int> vis(n,0);
+        return solve(arr,vis,start,n);
+    }
+};
