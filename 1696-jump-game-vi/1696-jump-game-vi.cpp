aa317c90b8089ef81a14ac07899f16f629ff32d5
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-09 16:07:10 +0530

    Time: 176 ms (97.90%), Space: 80.1 MB (77.60%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
-        priority_queue<pair<int,int>> pq;
-        pq.push({nums[n-1],n-1});
-        int curScore = nums[n-1];
-        for(int idx=n-2;idx>=0;idx--){
-            while(pq.top().second - idx > k) pq.pop();
-            int maxi = pq.top().first;
-            curScore = nums[idx] + maxi;
-            pq.push({curScore,idx});
+        deque<pair<int,int>> dq;
+        int curScore = 0;
+        for(int idx=n-1;idx>=0;idx--){
+            if(!dq.empty() and dq.front().second - idx > k) dq.pop_front();
+            curScore = nums[idx];
+            if(!dq.empty()) curScore += dq.front().first;
+            while(!dq.empty() and dq.back().first < curScore){
+                dq.pop_back();
+            }
+            dq.push_back({curScore,idx});
         }
         return curScore;
     }
