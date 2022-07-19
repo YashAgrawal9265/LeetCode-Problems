f5cbf75ad50db1ec5001d2f4eec91bf89eb1774d
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-19 19:40:54 +0530

    Time: 13 ms (32.46%), Space: 7.4 MB (85.78%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
@@ -1,52 +1,20 @@
 class Solution {
-private:
-    bool solve(string start, string end){
-        int n = start.size();
-        int i = 0; // pointing to end string
-        int j = 0; // pointing to start string
-        while(i < n){
-            if(end[i] == start[j]){
-                i++,j++;
-            }
-            else if(end[i] == 'R' and start[j] == 'L' or end[i] == 'L' and start[j] == 'R'){
-                return false;
-            }
-            else if(end[i] == 'X' and start[j] == 'L'){
-                return false;
-            }
-            else if(end[i] == 'R' and start[j] == 'X'){
-                return false;
-            }
-            else{
-                int k = j;
-                while(k < n){
-                    if(k < n and start[k] == end[i]){
-                        break;
-                    }
-                    k++;
-                }
-                if(k == n) return false;;
-                while(k != j){
-                    if(start[k-1] == 'X' and start[k] == 'L'){
-                        swap(start[k],start[k-1]);
-                    }
-                    else if(start[k-1] == 'R' and start[k] == 'X'){
-                        swap(start[k],start[k-1]);
-                    }
-                    else{
-                        return false;
-                    }
-                    k--;
-                }
-                j++;
-                i++;
-            } 
-        }
-        return true;
-    }
 public:
     bool canTransform(string start, string end) {
-        // cout<<solve(start,end)<<" "<<solve(end,start);
-        return solve(start,end);
+        int n = start.size();
+        int i = 0; // pointing to start string;
+        int j = 0; // poiting to end string;
+        while(i < n or j < n){
+            while(start[i] == 'X') i++;
+            while(end[j] == 'X') j++;
+            if(i == n and j == n) return true;
+            if(i == n or j == n) return false;
+            if(start[i] != end[j]) return false;
+            if(start[i] == 'L' and i < j) return false;
+            if(start[i] == 'R' and i > j) return false;
+            i++;
+            j++;
+        }
+        return true;
     }
 };
