997c47953652901cccbb7ab982f6a41233153cb0
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-22 23:19:47 +0530

    Time: 19 ms (46.34%), Space: 7.6 MB (51.96%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        vector<int> cur(n+1,0), next(n+1,0);
         
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<n;j++){
                 if(i > j) continue;
                 if(i == j){
-                    dp[i][j] = piles[i];
+                    cur[j] = piles[i];
                     continue;
                 }
-                int op1 = piles[i] - dp[i+1][j];
-                int op2 = piles[j] - dp[i][j-1];
-                dp[i][j] = max(op1,op2);
+                int op1 = piles[i] - next[j];
+                int op2 = piles[j] - cur[j-1];
+                cur[j] = max(op1,op2);
             }
+            next = cur;
         }
-        int aliceScore = dp[0][n-1];
+        int aliceScore = next[n-1];
         return aliceScore > 0;
     }
 };
