dfffabb4196f062487bbf84928e19e18ed3ed5d4
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-22 22:44:46 +0530

    Time: 404 ms (48.02%), Space: 132.1 MB (80.40%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& stoneValue, int idx, int n,vector<int>& dp){
-        if(idx >= n) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int score = 0;
-        int mxScore = -1e9;
-        for(int i=idx;i<idx+3 and i<n;i++){
-            score += stoneValue[i];
-            mxScore = max(mxScore,score - solve(stoneValue,i+1,n,dp));
-        }
-        return dp[idx] = mxScore;
-    }
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n+1,-1);
-        int aliceScore = solve(stoneValue,0,n,dp);
+        vector<int> dp(n+1,0);
+        for(int idx=n-1;idx>=0;idx--){
+            int score = 0;
+            int mxScore = -1e9;
+            for(int i=idx;i<idx+3 and i<n;i++){
+                score += stoneValue[i];
+                mxScore = max(mxScore,score - dp[i+1]);
+            }
+            dp[idx] = mxScore;
+        }
+        int aliceScore = dp[0];
         if(aliceScore == 0) return "Tie";
         return aliceScore > 0 ? "Alice" : "Bob";
     }
