7da986ea52d572ed37053ccd8c7fd7da913d7db4
Yash Agrawal <yashagrawal9265@gmail.com>
2022-07-28 10:38:10 +0530

    Time: 11 ms (70.38%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        if(s.size() != t.size()) return false;
+        vector<int> mp(26,0);
+        for(auto it: s) mp[it-'a']++;
+        for(auto it: t){
+            mp[it-'a']--;
+            if(mp[it-'a'] < 0) return false;
+        }
+        return true;
     }
 };
