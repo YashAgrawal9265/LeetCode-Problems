45bec061b339a3899c1738accbfda75a45e27583
Yash Agrawal <yashagrawal9265@gmail.com>
2022-08-01 12:05:02 +0530

    Time: 3 ms (44.81%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int totalPath = m+n-2;
+        int x = m + n - 2;
         int r = n-1;
         double result = 1;
         for(int i=1;i<=r;i++){
-            result *= (totalPath-r+i);
+            result *= (x - r + i);
             result /= i;
         }
-        return (int)result;
+        return result;
     }
 };
