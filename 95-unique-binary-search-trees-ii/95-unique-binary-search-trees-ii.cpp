3e221f51c880783e92bd0f6621b19fc9c21bbcd1
Yash Agrawal <yashagrawal9265@gmail.com>
2022-08-14 20:34:45 +0530

    Time: 26 ms (57.46%), Space: 16.1 MB (41.03%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -11,16 +11,16 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> helper(int st, int en){
+    vector<TreeNode*> solve(int st, int en){
         if(st > en){
             return {NULL};
         }
         vector<TreeNode*> cur;
         for(int i=st;i<=en;i++){
-            vector<TreeNode*> leftTree = helper(st,i-1);
-            vector<TreeNode*> rightTree = helper(i+1,en);
-            for(auto &l: leftTree){
-                for(auto &r: rightTree){
+            vector<TreeNode*> left = solve(st,i-1);
+            vector<TreeNode*> right = solve(i+1,en);
+            for(auto l: left){
+                for(auto r: right){
                     TreeNode* root = new TreeNode(i);
                     root->left = l;
                     root->right = r;
@@ -31,6 +31,6 @@ public:
         return cur;
     }
     vector<TreeNode*> generateTrees(int n) {
-         return helper(1,n);
+        return solve(1,n);
     }
 };
