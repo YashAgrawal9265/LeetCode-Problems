09416780dbf4c9cb736a360f1d1569644c2c8ad0
Yash Agrawal <yashagrawal9265@gmail.com>
2022-08-29 21:34:36 +0530

    Time: 254 ms (84.15%), Space: 102.1 MB (63.99%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
     int dp[100005][2];
-    int solve(vector<int>& nums1, vector<int>& nums2, int idx, int n, int swap){
+    int solve(vector<int>& nums1, vector<int>& nums2, int prev1, int prev2, int idx, int n, int swapped){
         if(idx == n) return 0;
-        if(dp[idx][swap]  != -1) return dp[idx][swap];
-        if(swap == 0){
-            int op1 = 1e8;
-            if(idx - 1 >= 0 and nums1[idx] > nums1[idx-1] and nums2[idx] > nums2[idx-1]){
-                op1 = solve(nums1,nums2,idx+1,n,0);
-            }
-            int op2 = 1e8;
-            if(idx-1>=0 and nums2[idx] > nums1[idx-1] and nums1[idx] > nums2[idx-1]) op2 = 1 + solve(nums1,nums2,idx+1,n,1);
-            return dp[idx][swap] = min(op1,op2);
-            
+        if(dp[idx][swapped] != -1) return dp[idx][swapped];
+        int swap = 1e8, noSwap = 1e8;
+        if(nums1[idx] > prev1 and nums2[idx] > prev2){
+            noSwap = solve(nums1,nums2,nums1[idx],nums2[idx],idx+1,n,0);
         }
-        else{
-            int op1 = 1e8;
-            if(idx-1 >= 0 and nums1[idx] > nums2[idx-1] and nums2[idx] > nums1[idx-1]){
-               op1 = solve(nums1,nums2,idx+1,n,0);
-            }
-            int op2 = 1e8;
-            
-            if(idx -1 >= 0 and nums2[idx] > nums2[idx-1] and nums1[idx] > nums1[idx-1]) op2 = 1 + solve(nums1,nums2,idx+1,n,1);
-            return dp[idx][swap] = min(op1,op2);
-            
+        if(nums2[idx] > prev1 and nums1[idx] > prev2){
+            swap = 1 + solve(nums1,nums2,nums2[idx],nums1[idx],idx+1,n,1);
         }
-        
+        return dp[idx][swapped] = min(swap,noSwap);
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         memset(dp,-1,sizeof(dp));
-        int x = solve(nums1,nums2,1,n,0);
-        int y = 1 + solve(nums1,nums2,1,n,1);
-        return min(x,y);
+        return solve(nums1,nums2,-1,-1,0,n,0);
     }
 };
