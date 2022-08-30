916dc8f911e9704d81aaa03b183fc906001d6d61
Yash Agrawal <yashagrawal9265@gmail.com>
2022-08-30 18:18:42 +0530

    Time: 88 ms (98.55%), Space: 39.4 MB (92.59%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    int sumSubarrayMins(vector<int>& nums) {
+    int sumSubarrayMins(vector<int>& arr) {
         int mod = 1e9 + 7;
-        int n = nums.size();
-        
-        // finding minimum element
-        // using next smaller and previous smaller
+        int n = arr.size();
+        long long sum = 0;
         stack<int> st;
-        long long result = 0;
         for(int i=0;i<=n;i++){
-            while(!st.empty() and (i == n || nums[st.top()] > nums[i])){
+            while(!st.empty() and (i == n or arr[i] <= arr[st.top()])){
+                int right = i;
+                int ele = arr[st.top()];
                 int idx = st.top();
-                int right = i - idx;
-                int val = nums[idx];
                 st.pop();
                 int left;
-                if(st.empty()){
-                    left = idx - (-1);
-                }
-                else{
-                    left = idx-st.top();
-                }
-                result = (result + 1LL*right*left*val) % mod;
+                if(st.empty()) left = -1;
+                else left = st.top();
+                int l = idx - left, r = right - idx;
+                sum = (sum + 1LL*l*r*ele) % mod;
             }
             st.push(i);
         }
-        return result;
+        return sum;
     }
 };
