0aa7f0d2234bea551250bac1c0722faf6b89bc86
Yash Agrawal <yashagrawal9265@gmail.com>
2022-09-01 18:55:26 +0530

    Time: 248 ms (5.07%), Space: 12.4 MB (50.72%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     int minInsertions(string str) {
         int n = str.size();
-        int open = 0, close = 0;
-        int result = 0;
         string s;
+        int open = 0, result = 0;
         for(int i=0;i<n;i++){
             if(str[i] == '('){
                 s = "(";
             }
             else{
                 s = "))";
-                if(i+1< n and str[i+1] == ')'){
+                if(i + 1 < n and str[i+1] == ')'){
                     i++;
                 }
                 else{
                     result += 1;
                 }
             }
-            if(s == "(") open++;
+            if(s == "("){
+                open++;
+            }
             else{
                 if(open > 0) open--;
-                else{
-                    result += 1;
-                }
+                else result += 1;
             }
         }
-        result += (open*2);
+        result += (2*open);
         return result;
     }
 };
