72d03016b3d87137f362dc6ee2e778f97a61dc04
Yash Agrawal <yashagrawal9265@gmail.com>
2022-09-11 01:17:39 +0530

    Time: 207 ms (24.87%), Space: 14.5 MB (69.89%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -20,18 +20,18 @@ public:
     int coinChange(vector<int> &coins, int tar)
     {
         int n = coins.size();
-        vector<int> next(tar + 1, 1e9), cur(tar+1,1e9);
-        next[0] = 0, cur[0] = 0;
+        vector<int> next(tar + 1, 1e9);
+        next[0] = 0;
         for(int idx = n-1;idx>=0;idx--){
             for(int target=0;target<=tar;target++){
                 int notake = next[target];
                 int take = 1e9;
                 if (target >= coins[idx])
-                    take = 1 + cur[target - coins[idx]];
+                    take = 1 + next[target - coins[idx]];
 
-                cur[target] = min(notake, take);
+                next[target] = min(notake, take);
             }
-            next = cur;
+           
         }
         
         int ans = next[tar];
