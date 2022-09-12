37d22e343fd183176d8e80ff9d7b05148edc1079
Yash Agrawal <yashagrawal9265@gmail.com>
2022-09-12 17:36:00 +0530

    Time: 25 ms (14.49%), Space: 7.5 MB (17.49%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int dp[1005][1005][3];
-    // greater = 1 means take greater element;
-    int solve(vector<int>& nums, int idx, int prev_idx, int n, int greater){
-        if(idx == n) return 0;
-        if(dp[idx][prev_idx+1][greater] != -1) return dp[idx][prev_idx+1][greater];
-        int notake = solve(nums,idx+1,prev_idx,n,greater);
-        int take = 0;
-        if(prev_idx == -1){
-            take = 1 + solve(nums,idx+1,idx,n,greater);
-        }
-        else{
-            if(greater and nums[idx] > nums[prev_idx]){
-                take = 1 + solve(nums,idx+1,idx,n,0);
-            }
-            else if(greater == 0 and nums[idx] < nums[prev_idx]){
-                take = 1 + solve(nums,idx+1,idx,n,1);
-            }
-        }
-        return dp[idx][prev_idx+1][greater] = max(take,notake);
-    }
     int wiggleMaxLength(vector<int>& nums) {
         int n = nums.size();
-        memset(dp,-1,sizeof(dp));
-        return max(solve(nums,0,-1,n,0), solve(nums,0,-1,n,1));
-       
+        int mxLen = 1;
+        vector<int> greater(n,1),smaller(n,1);
+        for(int i=1;i<n;i++){
+            for(int prev = 0; prev < i; prev++){
+                if(nums[i] > nums[prev]){
+                    greater[i] = max(greater[i],smaller[prev]+1);
+                }
+                else if(nums[i] < nums[prev]){
+                    smaller[i] = max(smaller[i],greater[prev]+1);
+                }
+            }
+            mxLen = max(greater[i],smaller[i]);
+        }
+        return mxLen;
     }
 };
