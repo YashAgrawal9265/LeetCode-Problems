a8777314a2e5b1f7acfc907716d4e51f3fa5f5d1
Yash Agrawal <yashagrawal9265@gmail.com>
2023-12-23 04:01:14 +0530

    Time: 27 ms (9.23%), Space: 22.7 MB (54.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,18 +5,18 @@ public:
         if(n == 1) return arr[0];
         if(arr[0] != arr[1]) return arr[0];
         if(arr[n-1] != arr[n-2]) return arr[n-1];
-        int low = 1, high = n-2;
-        while(high-low > 1){
-            int mid = (low + high) >> 1;
-            if(((mid&1) and arr[mid-1] == arr[mid]) or (!(mid&1) and arr[mid+1] == arr[mid])){
-                low = mid+1;
+        int l = 1, h = n-2;
+        while(h - l > 1){
+            int mid = (l + h) >> 1;
+            if(((mid&1) and arr[mid] == arr[mid+1]) or (!(mid&1) and arr[mid] == arr[mid-1])){
+                h = mid - 1;
             }
             else{
-                high = mid;
+                l = mid;
             }
         }
-        if(arr[low] != arr[low+1] and arr[low] != arr[low-1]) return arr[low];
-        if(arr[high] != arr[high+1] and arr[high] != arr[high-1]) return arr[high];
+        if(arr[l] != arr[l-1] and arr[l] != arr[l+1]) return arr[l];
+        if(arr[h] != arr[h-1] and arr[h] != arr[h+1]) return arr[h];
         return -1;
-    }
+     }
 };
