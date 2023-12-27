b9393b6dd6010ccd8c1abaece4919daae05fe980
Yash Agrawal <yashagrawal9265@gmail.com>
2023-12-26 21:44:52 -0600

    Time: 11 ms (66.23%), Space: 9.3 MB (40.54%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     string removeKdigits(string str, int k) {
         int n = str.size();
-        stack<int> st;
+        if(n == k) return "0";
+        if(k == 0) return str;
+        stack<char> st;
         for(int i=0;i<n;i++){
-            while(!st.empty() and st.top() > str[i]-'0' and k > 0){
+            while(!st.empty() and st.top() - '0' > str[i] - '0' and k > 0){
                 st.pop();
                 k--;
             }
             if(str[i] == '0' and st.empty()) continue;
-            st.push(str[i]-'0');
+            st.push(str[i]);
         }
-        while(!st.empty() and k--) st.pop();
+        while(k > 0 and !st.empty()){
+            st.pop();
+            k--;
+        }
         string result = "";
         while(!st.empty()){
-            result += (st.top() + '0');
+            result += st.top();
             st.pop();
         }
         if(result == "") return "0";
