11217b1e497f75e372567d991d78d4b5739fd6b2
Yash Agrawal <yashagrawal9265@gmail.com>
2024-01-27 12:52:03 -0600

    Time: 0 ms (100.00%), Space: 8.3 MB (15.10%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,12 +3,12 @@ private:
     void solve(vector<int>& temp, vector<vector<int>>& result, int target, int size, int idx, int n){
         
         if(idx > n){
-            if(target == 0 and temp.size() == size) result.push_back(temp);
+            if(target == 0 and size == 0) result.push_back(temp);
             return;
         }
-        if(target - idx >= 0 and temp.size() < size){
+        if(target - idx >= 0 and size > 0){
             temp.push_back(idx);
-            solve(temp,result,target-idx,size,idx+1,n);
+            solve(temp,result,target-idx,size-1,idx+1,n);
             temp.pop_back();
         }
         solve(temp,result,target,size,idx+1,n);
