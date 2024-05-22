7a462f7869ffda2ca6332bb815dcf0f6db938249
Yash Agrawal <yashagrawal9265@gmail.com>
2024-05-21 20:47:54 -0500

    Time: 0 ms (100.00%), Space: 7.8 MB (95.93%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     string removeOuterParentheses(string str) {
-        int n = str.size();
-        vector<int> mp(n+1,0);
-        int st = 0;
-        int open = 0, close = 0;
-        for(int i=0;i<n;i++){
-            if(str[i] == '(') open++;
-            else if(str[i] == ')') close++;
-            if(open == close and open != 0 and close != 0){
-                mp[st] = 1;
-                mp[i] = 1;
-                st = i+1;
-                open = 0;
-                close = 0;
-            }
-        }
+        int open = 0;
         string result = "";
-        for(int i=0;i<n;i++){
-            if(mp[i] == 0) result += str[i];
+        for(int i=0;i<str.size();i++){
+            if((str[i] == '(' and open > 0) or (str[i] == ')' and open > 1)) result += str[i];
+            
+            if(str[i] == '(') open++;
+            else if(str[i] == ')') open--;
         }
         return result;
+        
     }
 };
