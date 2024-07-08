ac1b44734e88cde7cd7a23316a92f02ddbe3521f
Yash Agrawal <yashagrawal9265@gmail.com>
2024-07-08 17:01:09 -0500

    Time: 89 ms (47.98%), Space: 47.5 MB (29.89%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,3 +1,31 @@
+class DisjointSet{
+private:
+    vector<int> parent,size;
+public:
+    DisjointSet(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1,0);
+        for(int i=0;i<=n;i++) parent[i] = i;
+    }
+    int findParent(int u){
+        if(parent[u] == u) return u;
+        return parent[u] = findParent(parent[u]);
+    }
+    void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+        
+};
+
 class Solution {
 private:
     void dfs(vector<int> adj[], vector<int>&vis, int node){
@@ -12,20 +40,17 @@ public:
         if(connections.size() < n-1) return -1;
         
         // find the number of components;
-        vector<int> adj[n];
+        
+        DisjointSet dt(n);
         for(auto it: connections){
-            int u = it[0], v= it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            if(dt.findParent(it[0]) != dt.findParent(it[1]))
+                dt.unionBySize(it[0],it[1]);
         }
-        int components = 0;
-        vector<int> vis(n,0);
+        unordered_set<int> st;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                components++;
-                dfs(adj,vis,i);
-            }
+            st.insert(dt.findParent(i));
         }
-        return components-1;
+        return st.size() - 1;
+         
     }
 };
