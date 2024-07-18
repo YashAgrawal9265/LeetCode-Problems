71e2dd7aae04afde600362e827a750b011445ce0
Yash Agrawal <yashagrawal9265@gmail.com>
2024-07-18 14:34:54 -0500

    Time: 8 ms (59.78%), Space: 9.8 MB (7.40%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -9,8 +9,6 @@ public:
             mxFreq = max(mxFreq,mp[str[r]]);
             while((r-l+1) - mxFreq > k){
                 mp[str[l]]--;
-                mxFreq = 0;
-                for(auto it: mp) mxFreq = max(mxFreq,it.second);
                 l++;
             }
             ans = max(ans,r-l+1);
