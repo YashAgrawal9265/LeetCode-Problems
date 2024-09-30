40e94c9f89c5ea2c12397b6fcd85b0c780cc6a7b
Yash Agrawal <yashagrawal9265@gmail.com>
2024-09-30 18:04:02 -0500

    Time: 14 ms (75.81%), Space: 17.4 MB (56.58%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,41 +1,29 @@
-struct ele{
-    int stops,node,distance;
-    ele(int stops, int node, int distance){
-        this->node = node;
-        this->distance = distance;
-        this->stops = stops;
-    }
-};
-
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<pair<int,int>> adj[n];
         for(auto it: flights){
-            int u = it[0], v = it[1], wt = it[2];
+            int u = it[0], v= it[1], wt = it[2];
             adj[u].push_back({v,wt});
         }
-    
         vector<int> dist(n,1e8);
-        queue<ele> q;
-        q.push({0,src,0});
+        queue<pair<int,pair<int,int>>> pq;
+        
+        pq.push({0,{0,src}});
         dist[src] = 0;
-        while(!q.empty()){
-            int node = q.front().node;
-            int distance = q.front().distance;
-            int stops = q.front().stops;
-            q.pop();
-            if(stops > k) continue;
+        while(!pq.empty()){
+            int node = pq.front().second.second;
+            int distance = pq.front().second.first;
+            int stops = pq.front().first;
+            pq.pop();
             for(auto it: adj[node]){
                 int childNode = it.first;
-                int wt = it.second;
-                if(distance + wt < dist[childNode] and stops <= k){
-                    dist[childNode] = distance + wt;
-                    q.push({stops+1,childNode,dist[childNode]});
-                    
+                int childWt = it.second;
+                if(distance + childWt < dist[childNode] and stops <= k){
+                    dist[childNode] = distance + childWt;
+                    pq.push({stops+1,{dist[childNode],childNode}});
                 }
             }
-            
         }
         return dist[dst] >= 1e8 ? -1 : dist[dst];
     }
