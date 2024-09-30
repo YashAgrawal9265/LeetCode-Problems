670ccd3c681e5a4033acaa950cd5cca1c67e0620
Yash Agrawal <yashagrawal9265@gmail.com>
2024-09-30 16:19:19 -0500

    Time: 164 ms (19.50%), Space: 42.6 MB (16.24%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,49 +1,29 @@
-struct ele{
-   int x,y,wt;
-    ele(int a, int b, int c){
-        x = a;
-        y = b;
-        wt = c;
-    }
-};
-struct cmp{
-    bool operator()(ele a, ele b){
-        return a.wt > b.wt;
-    }
-};
-
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int m = heights.size(), n = heights[0].size();
-        int xdir[4] = {-1,0,1,0};
-        int ydir[4] = {0,1,0,-1};
-        
+    int minimumEffortPath(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
         vector<vector<int>> dist(m,vector<int>(n,1e8));
-        priority_queue<ele,vector<ele>,cmp> pq;
+        int xdir[] = {-1,0,1,0};
+        int ydir[] = {0,1,0,-1};
+        queue<pair<int,pair<int,int>>> q;
         dist[0][0] = 0;
-        pq.push(ele(0,0,0));
-        while(!pq.empty()){
-            int x = pq.top().x;
-            int y = pq.top().y;
-            int wt = pq.top().wt;
-            pq.pop();
-            if(x == m-1 and y == n-1) return wt;
+        q.push({0,{0,0}});
+        while(!q.empty()){
+            int node = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            q.pop();
             for(int i=0;i<4;i++){
                 int nx = x + xdir[i];
                 int ny = y + ydir[i];
                 if(nx >= 0 and ny >= 0 and nx < m and ny < n){
-                    int mxDiff = max(dist[x][y],abs(heights[x][y]-heights[nx][ny]));
-                    if(mxDiff < dist[nx][ny]){
-                        dist[nx][ny] = mxDiff;
-                        pq.push(ele(nx,ny,mxDiff));
+                    if(max(dist[x][y],abs(mat[nx][ny] - mat[x][y])) < dist[nx][ny]){
+                        dist[nx][ny] = max(dist[x][y],abs(mat[nx][ny] - mat[x][y]));
+                        q.push({dist[nx][ny],{nx,ny}});
                     }
                 }
-                
-                
             }
         }
-        
-        return -1;
+        return dist[m-1][n-1];
     }
 };
