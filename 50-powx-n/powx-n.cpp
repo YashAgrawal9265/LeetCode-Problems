f4e445a1b9bd3602ce7caf3e3151b1ac28e508a8
Yash Agrawal <yashagrawal9265@gmail.com>
2025-03-24 12:31:18 -0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.64%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,11 +1,30 @@
 class Solution {
 public:
     double solve(double base, long power){
-        if(power == 1) return base;
-        if(power%2 == 0){
-            return solve(base*base,power/2);
+        // if(power == 1) return base;
+        // if(power%2 == 0){
+        //     return solve(base*base,power/2);
+        // }
+        // return base*solve(base,power-1);
+
+        double result = 1;
+        while(power){
+            // if(power%2 == 1){
+            //     result *= base;
+            //     power -= 1;
+            // }
+            // base *= base;
+            // power /= 2;
+            if(power&1){
+                result *= base;
+                power -= 1;
+            }
+            else{
+                base *= base;
+                power /= 2;
+            }
         }
-        return base*solve(base,power-1);
+        return result;;
     }
     double myPow(double base, int power) {
         if(power == 0) return 1;
