2efb69bc6f6f3f35ac864dc8ccf2b1dcef8c4645
Yash Agrawal <yashagrawal9265@gmail.com>
2025-06-29 12:00:34 -0500

    Time: 0 ms (100.00%) | Memory: 52.2 MB (63.84%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n = nums.size();
+        int mxJump = 0;
+        for(int i=0;i<n;i++){
+            if(i > mxJump) return false;
+            mxJump = max(mxJump,i+nums[i]);
+        }
+        return true;
+    }
+};
