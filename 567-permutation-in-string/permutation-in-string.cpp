c15f34d62a6403ac9c8bcd07ff689aeed5c6826b
Yash Agrawal <yashagrawal9265@gmail.com>
2025-08-24 16:47:12 -0700

    Time: 0 ms (100.00%) | Memory: 10 MB (66.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -4,19 +4,29 @@ public:
         vector<int> s1Mp(26, 0), s2Mp(26,0);
         if(s2.size() < s1.size()) return false;
         int l = 0, r = 0;
+        int matches = 0;
         while(r < s1.size()){
             s1Mp[s1[r]-'a']++;
             s2Mp[s2[r]-'a']++;
             r++;
         }
-        if(s1Mp == s2Mp) return true;
+        for(int i=0;i<26;i++){
+            if(s1Mp[i] == s2Mp[i]) matches++;
+        }
         while(r < s2.size()){
-            s2Mp[s2[r] - 'a']++;
-            s2Mp[s2[l] - 'a']--;
-            if(s1Mp == s2Mp) return true;
+            if(matches == 26) return true;
+            int index = s2[r] - 'a';
+            s2Mp[index]++;
+            if(s1Mp[index] == s2Mp[index]) matches++;
+            else if(s1Mp[index]+1 == s2Mp[index]) matches--;
             r++;
+
+            index = s2[l] - 'a';
+            s2Mp[index]--;
+            if(s1Mp[index] == s2Mp[index]) matches++;
+            else if(s1Mp[index]-1 == s2Mp[index]) matches--;
             l++;
         }
-        return false;
+        return matches == 26;
     }
 };
