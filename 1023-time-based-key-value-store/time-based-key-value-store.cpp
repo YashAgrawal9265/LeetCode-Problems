6ea17519671dbdefc99880ee259e6295ca7a0be0
Yash Agrawal <yashagrawal9265@gmail.com>
2025-10-18 13:01:31 -0700

    Time: 36 ms (88.67%) | Memory: 136.6 MB (80.73%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,9 +1,23 @@
 class TimeMap {
 private:
     unordered_map<string,vector<pair<int,string>>> mp;
+    int upper_bound(vector<pair<int,string>>& arr, int key){
+        int l = 0, h = arr.size() - 1;
+        while(h - l > 1){
+            int mid = (l + h) >> 1;
+            if(arr[mid].first > key){
+                h = mid;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        if(arr[l].first > key) return l;
+        if(arr[h].first > key) return h;
+        return arr.size();
+    }
 public:
     TimeMap() {
-        
         mp.clear();
     }
     
@@ -12,21 +26,12 @@ public:
     }
     
     string get(string key, int timestamp) {
+        string ans = "";
         if(mp.find(key) == mp.end()) return "";
-        int low = 0, high = mp[key].size() - 1;
-        while(high - low > 1){
-            int mid = (low + high) / 2;
-            if(mp[key][mid].first <= timestamp){
-                low = mid;
-            }
-            else{
-                high = mid - 1;
-            }
-
-        }
-        if(mp[key][high].first <= timestamp) return mp[key][high].second;
-        if(mp[key][low].first <= timestamp) return mp[key][low].second;
-        return "";
+        int idx = upper_bound(mp[key],timestamp);
+        idx--;
+        if(idx >= 0 and idx < mp[key].size()) ans = mp[key][idx].second;
+        return ans;
     }
 };
 
