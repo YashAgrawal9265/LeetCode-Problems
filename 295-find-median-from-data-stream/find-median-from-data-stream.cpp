384adc6c926661158cdb3a41600ec3d0209c4f4f
Yash Agrawal <yashagrawal9265@gmail.com>
2025-10-23 21:48:54 -0700

    Time: 67 ms (70.07%) | Memory: 148.7 MB (11.32%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,40 +1,34 @@
 class MedianFinder {
+priority_queue<int> mxHeap;
+priority_queue<int,vector<int>,greater<int>> mnHeap;
 public:
-    // minHeap
-    priority_queue<int,vector<int>,greater<int>> mnHeap;
-
-    // maxHeap
-    priority_queue<int> mxHeap;
     MedianFinder() {
-    
+        while(!mxHeap.empty()) mxHeap.pop();
+
+        while(!mnHeap.empty()) mnHeap.pop();
     }
     
     void addNum(int num) {
-        if(mxHeap.empty() || num < mxHeap.top()){
-            mxHeap.push(num);
-        }
-        else{
-            mnHeap.push(num);
-        }
-
-        if(mxHeap.size() > mnHeap.size() + 1){
+        
+        if(mxHeap.empty() || num <= mxHeap.top()) mxHeap.push(num);
+        else mnHeap.push(num);
+        
+        if((int)mxHeap.size() - (int)mnHeap.size()> 1){
+           
             mnHeap.push(mxHeap.top());
             mxHeap.pop();
         }
-        else if(mnHeap.size() > mxHeap.size() + 1){
+        
+        if((int)mnHeap.size() - (int)mxHeap.size() > 0){
+            
             mxHeap.push(mnHeap.top());
             mnHeap.pop();
         }
     }
     
     double findMedian() {
-        if(mxHeap.size() == mnHeap.size()){
-            return double(mxHeap.top() + mnHeap.top())/2.0;
-        }
-        else{
-            if(mxHeap.size() > mnHeap.size()) return mxHeap.top();
-            else return mnHeap.top();
-        }
+        if((mnHeap.size() + mxHeap.size())&1) return mxHeap.top();
+        return (mnHeap.top() + mxHeap.top()) / 2.0;
     }
 };
 
