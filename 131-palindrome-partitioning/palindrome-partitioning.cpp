cc53631a80e9778de6e4704cf9b9c14e9221079d
Yash Agrawal <yashagrawal9265@gmail.com>
2025-10-25 22:09:27 -0700

    Time: 40 ms (65.50%) | Memory: 57.6 MB (58.83%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool isPali(string& str, int i, int j){
-        while(i < j){
+        while(i <= j){
             if(str[i] != str[j]) return false;
             i++,j--;
         }
         return true;
     }
-    void solve(string& str, int idx, int n, vector<vector<string>>& result, vector<string>& temp){
-        if(idx == n){
+    void solve(string str, int idx, int n, vector<string>& temp, vector<vector<string>>& result){
+        if(idx >= n){
             result.push_back(temp);
             return;
         }
         for(int i=idx;i<n;i++){
             if(isPali(str,idx,i)){
                 temp.push_back(str.substr(idx,i-idx+1));
-                solve(str,i+1,n,result,temp);
+                solve(str,i+1,n,temp,result);
                 temp.pop_back();
             }
         }
     }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
+    vector<vector<string>> partition(string str) {
+        int n= str.size();
         vector<string> temp;
-        solve(s,0,s.size(),result,temp);
+        vector<vector<string>> result;
+        solve(str,0,n,temp,result);
         return result;
     }
 };
