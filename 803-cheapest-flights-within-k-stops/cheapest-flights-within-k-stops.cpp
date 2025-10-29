a44a2bd5ce4417a7cdfadb05df620a5a7421b1f1
Yash Agrawal <yashagrawal9265@gmail.com>
2025-10-28 19:13:16 -0700

    Time: 7 ms (28.24%) | Memory: 18.1 MB (58.14%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<pair<int,int>> adj[n];
+         vector<pair<int,int>> adj[n];
 
         for(auto it: flights){
             int u = it[0], v= it[1], wt = it[2];
             adj[u].push_back({v,wt});
         }
-        
-        k++;
-        vector<int> dist(n,1e8);
-        // priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
-        queue<pair<int,pair<int,int>>> pq;
-        pq.push({0,{0,src}});
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+       
+        k += 1;
+        vector<int> dist(n,1e9);
         dist[src] = 0;
+        // // {k,node,distance};
+        pq.push({0,{src,0}});
+
         while(!pq.empty()){
-            int stops = pq.front().first;
-            int curDist = pq.front().second.first;
-            int node = pq.front().second.second;
+            int node = pq.top().second.first;
+            int curDist = pq.top().second.second;
+            int stops = pq.top().first;
             pq.pop();
+            
             for(auto it: adj[node]){
-                int childNode= it.first, childWt = it.second;
-                if(stops + 1 <= k and curDist + childWt < dist[childNode]){
-                    dist[childNode] = curDist + childWt;
-                    pq.push({stops+1,{dist[childNode],childNode}});
+                int wt = it.second;
+                int childNode = it.first;
+                if(curDist + wt < dist[childNode] and (stops + 1) <= k){
+                    dist[childNode] = curDist + wt;
+                    pq.push({stops+1,{childNode,dist[childNode]}});
                 }
             }
         }
-        return dist[dst] >= 1e8 ? -1 : dist[dst];
+        // for(auto it: dist) cout<<it<<" ";
+        return dist[dst] >= 1e9 ? -1 : dist[dst];
+
     }
 };
