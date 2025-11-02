21163a24ebe6f881907d8bf69f22f528d852802f
Yash Agrawal <yashagrawal9265@gmail.com>
2025-11-02 15:53:42 -0800

    Time: 0 ms (100.00%) | Memory: 22.2 MB (40.76%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& arr) {
+        vector<int> result;
         stack<int> st;
-
-        int n = arr.size();
-        int i = 0;
-        while(i < n){
+        int i  = 0;
+        while(i < arr.size()){
             if(st.empty()){
                 st.push(arr[i]);
                 i++;
             }
             else{
-                if((st.top() > 0 and arr[i] > 0) || (st.top() < 0 and arr[i] < 0)){
+                if((st.top() > 0 and arr[i] > 0 )|| (st.top() < 0 and arr[i] < 0)){
                     st.push(arr[i]);
                     i++;
                 }
-                else{
-                    // if top is positive and array element is negative
-                    if(st.top() > 0 and arr[i] < 0){
-                        if(abs(arr[i]) == st.top()){
-                            st.pop();
-                            i++;
-                        }
-                        else if(abs(arr[i]) > st.top()){
-                            st.pop();
-                        }
-                        else{
-                            i++;
-                        }
-                    }
-                    else if(st.top() < 0 and arr[i] > 0){
-                        st.push(arr[i]);
+                // if top element is positive and array element is negative
+                else if(st.top() > 0 and arr[i] < 0){
+                    if(st.top() > abs(arr[i])) i++;
+                    else if(st.top() < abs(arr[i])) st.pop();
+                    else{
+                        st.pop();
                         i++;
                     }
                 }
+                else if(st.top() < 0 and arr[i] > 0){
+                   st.push(arr[i]);
+                   i++;
+                }
             }
         }
-        vector<int> result;
         while(!st.empty()){
             result.push_back(st.top());
             st.pop();
