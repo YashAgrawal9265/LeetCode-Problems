67a84a376282d0057952c26cbc53a4829bd06c71
Yash Agrawal <yashagrawal9265@gmail.com>
2025-11-30 21:44:58 -0800

    Time: 78 ms (39.52%) | Memory: 21 MB (66.94%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,50 +1,28 @@
 class Solution:
     def calculate(self, str: str) -> int:
-
         n = len(str)
-        stack = []
-        opr = '+'
+        st = []
         curNum = 0
-        lastNum = 0
-        result = 0
+        opr = '+'
 
         for i in range(n):
             ch = str[i]
-            if ch.isdigit():
+            if(ch.isdigit()):
                 curNum = curNum*10 + int(ch)
-            
-            if (not ch.isdigit() and ch != ' ') or i == n-1:
-               
-                if opr == '+':
-                    # print('+')
-                    result += lastNum
-                    lastNum = curNum
-                    # stack.append(curNum)
-                elif opr == '-':
-                    # print('e-')
-                    result += lastNum
-                    lastNum = -curNum
-                    # stack.append(-curNum)
-                elif opr == '*':
-                    # print('*')
-                    lastNum = lastNum * curNum
-                    # stack.append(stack.pop() * curNum)
-                elif opr == '/':
-                    # print('/')
-                    # stack.append(int(stack.pop() / curNum))
-                    lastNum = int(lastNum / curNum)
-
+            if(not ch.isdigit() and ch != ' ' or i == n-1):
+                if(opr == '+'):
+                    st.append(curNum)
+                if(opr == '-'):
+                    st.append(-curNum)
+                if(opr == '*'):
+                    st.append(st.pop() * curNum)
+                if(opr == '/'):
+                    st.append(int(st.pop() / curNum))
                 curNum = 0
                 opr = ch
         
-        result += lastNum
+        result = 0
+        while(st):
+            result += st.pop()
         return result
-     
-        # while stack:
-        #     result += stack.pop()
-
-        # return result
-
-            
-
         
