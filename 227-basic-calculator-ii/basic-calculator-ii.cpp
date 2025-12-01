aa946aa52b26bc721da960e1df3b6c2cf9c57a08
Yash Agrawal <yashagrawal9265@gmail.com>
2025-11-30 18:10:57 -0800

    Time: 7 ms (67.71%) | Memory: 15.1 MB (49.74%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,38 +1,84 @@
+// class Solution {
+// public:
+//     int calculate(string str) {
+//         int n= str.size();
+//         int lastNum = 0;
+//         int curNum = 0;
+//         char opr = '+';
+//         int result = 0;
+//         for(int i=0;i<n;i++){
+//             if(str[i] >= '0' and str[i] <= '9'){
+//                 curNum = curNum*10 + (str[i] - '0');
+//             }
+//             if(!isdigit(str[i]) && str[i] != ' ' or i == (n-1)){
+//                 char curOpr = str[i];
+//                 if(opr == '+'){
+//                     result += lastNum;
+//                     lastNum = curNum;
+//                 }
+//                 else if(opr == '-'){
+//                     result += lastNum;
+//                     lastNum = -curNum;
+//                 }
+//                 else if(opr == '*'){
+//                     lastNum =  lastNum * curNum;
+                    
+//                 }
+//                 else if(opr == '/'){
+//                     lastNum = lastNum / curNum;
+//                 }
+//                 curNum = 0;
+//                 opr = curOpr;
+//             }
+
+//         }
+//         result += lastNum;
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     int calculate(string str) {
         int n= str.size();
-        int lastNum = 0;
+        stack<int> st;
         int curNum = 0;
-        char opr = '+';
-        int result = 0;
+        char curOpr = '+';
         for(int i=0;i<n;i++){
             if(str[i] >= '0' and str[i] <= '9'){
                 curNum = curNum*10 + (str[i] - '0');
+               
             }
             if(!isdigit(str[i]) && str[i] != ' ' or i == (n-1)){
-                char curOpr = str[i];
-                if(opr == '+'){
-                    result += lastNum;
-                    lastNum = curNum;
+                if(curOpr == '+'){
+                    st.push(curNum);
                 }
-                else if(opr == '-'){
-                    result += lastNum;
-                    lastNum = -curNum;
+                else if(curOpr == '-'){
+                    st.push(-curNum);
                 }
-                else if(opr == '*'){
-                    lastNum =  lastNum * curNum;
+                else if(curOpr == '*'){
+                    int num = st.top() * curNum;
+                    st.pop();
                     
+                    st.push(num);
                 }
-                else if(opr == '/'){
-                    lastNum = lastNum / curNum;
+                else if(curOpr == '/'){
+                    int num = st.top() / curNum;
+                    st.pop();
+                    st.push(num); 
                 }
                 curNum = 0;
-                opr = curOpr;
+                curOpr = str[i];
             }
 
+            
         }
-        result += lastNum;
+        int result = 0;
+        while(!st.empty()){
+           
+            result += st.top();
+            st.pop();
+        }
         return result;
     }
 };
