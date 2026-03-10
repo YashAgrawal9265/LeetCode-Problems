6743373a7dd2548aeaba5b09da57603ab36ad378
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-10 13:39:01 -0700

    Time: 0 ms (100.00%) | Memory: 10.1 MB (12.29%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& arr) {
+        int n = arr.size();
+        int maxi = 0;
+        for(auto it: arr){
+            maxi = max(maxi,it);
+        }
+        vector<int> buckets(maxi+1,0);
+        for(auto it: arr){
+            buckets[it]++;
+        }
+
+        int first = maxi, second = maxi;
+        while(first > 0){
+            if(buckets[first] %2 == 0){
+                first--;
+                continue;
+            }
+            int j = min(first-1,second);
+            while(j >=0 and buckets[j] == 0){
+                j--;
+            }
+            if(j <= 0) return first;
+            second = j;
+            buckets[second]--;
+            buckets[first]--;
+            buckets[first - second]++;
+            first = max(second,first-second);
+        }
+        return 0;
+    }
+};
