f366935ac213f9eab204c540fb8301289529dc9a
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-13 21:18:25 -0700

    Time: 48 ms (52.29%) | Memory: 57.5 MB (58.02%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,33 +1,30 @@
 class Solution {
-
 public:
-    bool isPali(string& str, int i, int j){
-        while(i <= j){
-            if(str[i] != str[j]) return false;
-            i++,j--;
+    bool isPali(string& str, int st, int en){
+        while(st <= en){
+            if(str[st] != str[en]) return false;
+            st++;
+            en--;
         }
         return true;
     }
-    void solve(string str, int idx, int n, vector<string>& temp, vector<vector<string>>& result, vector<vector<int>>& dp){
+    void solve(string str, int idx, int n, vector<string>& temp, vector<vector<string>>& result){
         if(idx >= n){
             result.push_back(temp);
             return;
         }
         for(int i=idx;i<n;i++){
-            if(str[i] == str[idx] and (i-idx <=2 || dp[idx+1][i-1])){
-                dp[idx][i] = 1;
+            if(isPali(str,idx,i)){
                 temp.push_back(str.substr(idx,i-idx+1));
-                solve(str,i+1,n,temp,result,dp);
+                solve(str,i+1,n,temp,result);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string str) {
-        int n= str.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
-        vector<string> temp;
         vector<vector<string>> result;
-        solve(str,0,n,temp,result,dp);
+        vector<string> temp;
+        solve(str,0,str.size(),temp,result);
         return result;
     }
 };
