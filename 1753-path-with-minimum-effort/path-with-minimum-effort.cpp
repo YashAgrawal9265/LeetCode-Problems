c7e987960685089cbbdea7cb7b4bfe271255f62c
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-17 23:05:27 -0700

    Time: 52 ms (61.30%) | Memory: 24.4 MB (91.38%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m,vector<int>(n,1e8));
+        int xdir[] = {-1,0,1,0};
+        int ydir[] = {0,1,0,-1};
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> q;
+        dist[0][0] = 0;
+        q.push({0,{0,0}});
+        while(!q.empty()){
+            int node = q.top().first;
+            int x = q.top().second.first;
+            int y = q.top().second.second;
+            q.pop();
+            if(x == m-1 and y == n-1) return dist[x][y];
+            for(int i=0;i<4;i++){
+                int nx = x + xdir[i];
+                int ny = y + ydir[i];
+                if(nx >= 0 and ny >= 0 and nx < m and ny < n){
+                    if(max(dist[x][y],abs(mat[nx][ny] - mat[x][y])) < dist[nx][ny]){
+                        dist[nx][ny] = max(dist[x][y],abs(mat[nx][ny] - mat[x][y]));
+                        q.push({dist[nx][ny],{nx,ny}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
