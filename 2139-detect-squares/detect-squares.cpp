bf2017c9fbd4271b180d6a839e27cef4a1041d2e
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-28 22:16:06 -0700

    Time: 73 ms (20.40%) | Memory: 101.4 MB (84.96%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.cpp
@@ -0,0 +1,33 @@
+class DetectSquares {
+public:
+    map<pair<int,int>,int> mp;
+    vector<pair<int,int>> arr;
+    DetectSquares() {
+        
+    }
+    
+    void add(vector<int> point) {
+        arr.push_back({point[0],point[1]});
+        mp[{point[0],point[1]}]++;
+    }
+    
+    int count(vector<int> point) {
+        int count  = 0;
+        for(auto it: arr){
+            int x = it.first, y = it.second;
+            int px = point[0], py = point[1];
+            if((abs(px - x) != abs(py - y) ) or (px == x and py == y)) continue;
+            int c1 = mp[{px,y}];
+            int c2 = mp[{x,py}];
+            count += (c1*c2);
+        }
+        return count;
+    }
+};
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares* obj = new DetectSquares();
+ * obj->add(point);
+ * int param_2 = obj->count(point);
+ */
