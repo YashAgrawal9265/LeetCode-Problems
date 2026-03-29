f4f224c72fa5e5ddd99128a3471191af0664a9fe
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-28 22:21:02 -0700

    Time: 47 ms (35.18%) | Memory: 101.3 MB (89.45%) - LeetSync

--- 2139-detect-squares/detect-squares.cpp
+++ 2139-detect-squares/detect-squares.cpp
@@ -1,14 +1,19 @@
 class DetectSquares {
 public:
-    map<pair<int,int>,int> mp;
+    // map<pair<int,int>,int> mp;
+    unordered_map<long,int> mp;
     vector<pair<int,int>> arr;
+    long getKey(int x, int y){
+        return (((1LL*x) << 32) | (1LL*y));
+    }
     DetectSquares() {
         
     }
     
     void add(vector<int> point) {
         arr.push_back({point[0],point[1]});
-        mp[{point[0],point[1]}]++;
+        // mp[{point[0],point[1]}]++;
+        mp[getKey(point[0],point[1])]++;
     }
     
     int count(vector<int> point) {
@@ -17,8 +22,10 @@ public:
             int x = it.first, y = it.second;
             int px = point[0], py = point[1];
             if((abs(px - x) != abs(py - y) ) or (px == x and py == y)) continue;
-            int c1 = mp[{px,y}];
-            int c2 = mp[{x,py}];
+            // int c1 = mp[{px,y}];
+            int c1 = mp[getKey(px,y)];
+            // int c2 = mp[{x,py}];
+            int c2 = mp[getKey(x,py)];
             count += (c1*c2);
         }
         return count;
