2c1e77f42807308f9c3718d9f9c7cce103485550
Yash Agrawal <yashagrawal9265@gmail.com>
2026-03-29 16:36:52 -0700

    Time: 4 ms (0.86%) | Memory: 9.4 MB (3.05%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int solve(int n){
+    int solve(int n,vector<int>& dp){
         if(n == 1) return 0;
+        if(dp[n] != -1) return dp[n];
         if(n&1){
-            return (n-1)/2 + solve((n-1)/2 + 1);
+            return dp[n] = (n-1)/2 + solve((n-1)/2 + 1,dp);
         }
-        else return n/2 + solve(n/2);
+        else return dp[n] = n/2 + solve(n/2,dp);
     }
     int numberOfMatches(int n) {
-        return solve(n);
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
